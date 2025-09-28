659b507c28e3f730e759d7f1ac39e6b16a37c3de
Rodrigo Leite <leiteofc.dev@gmail.com>
2025-09-28 18:40:10 -0300

    01

--- Sem05-T1/Sem05-T1-Ex01.py
+++ Sem05-T1/Sem05-T1-Ex01.py
@@ -1,13 +1,17 @@
+"""01. Escreva um programa que ler três valores inteiros (a, b, e c).
+Calcule o mostre o resultado da função:"""
 
+#função que calcula expressão desejada.
 def funcao(a, b, c):
     func = (2 * a) + (5 * b) - c
     return func
 
+#Bloco Principal
 def main():
 
-    numa = int(input())
-    numb = int(input())
-    numc = int(input())
+    numa = int(input("Insira o valor de A:"))
+    numb = int(input("Insira o valor de B:"))
+    numc = int(input("Insira o valor de C:"))
 
     print(funcao(numa, numb, numc))
 
